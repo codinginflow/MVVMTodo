a1b4618b911aaebee4cbae88cd53845d8a67fc35
Florian <info@codinginflow.com>
2020-11-11 11:34:24 +0100

    End of part 2

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/Task.kt
@@ -0,0 +1,20 @@
+package com.codinginflow.mvvmtodo.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    val name: String,
+    val important: Boolean = false,
+    val completed: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable {
+    val createdDateFormatted: String
+        get() = DateFormat.getDateTimeInstance().format(created)
+}
