455eab5131e02e8a9c4e5eabe141560085d56f04
Florian <info@codinginflow.com>
2020-11-12 11:40:50 +0100

    End of part 3

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codinginflow.mvvmtodo
+package com.codinginflow.mvvmtodo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codinginflow.mvvmtodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.codinginflow.mvvmtodo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.mvvmtodo.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
