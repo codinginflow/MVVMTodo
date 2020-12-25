ae0264e4d8bee718475b6a7237874d4bf455d76b
ravi patel <ravi.patel@bell.ca>
2020-12-25 16:53:13 +0530

    Added navigation with nav graph for fragment.

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,9 @@
+package com.codinginflow.mvvmtodo.ui.tasks
+
+import androidx.annotation.Keep
+import androidx.fragment.app.Fragment
+import com.codinginflow.mvvmtodo.R
+
+@Keep
+class TasksFragment() : Fragment(R.layout.fragment_task) {
+}
